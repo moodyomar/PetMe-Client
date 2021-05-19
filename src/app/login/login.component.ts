0c4163679a4966ac99260de55239e73bee3cbae7
moodyomar <lonak.secret@gmail.com>
2021-05-19 19:37:16 +0300

    add login & signup forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  @ViewChild('f') myForm:any
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  async onSub(){
+    if(this.myForm.form.status == "VALID"){
+      // success
+      let formData = this.myForm.form.value;
+      try{
+        // let data = await this.authSer.logInFb(formData.email,formData.password);
+        // deleting firebase configiruation and placing a test ones
+        let data = {user:'empty test'}
+        // success log in
+        if(data.user){
+          // TODO : redirect to admin
+          // localStorage.setItem("fb_user",data.user.uid)
+          // this.route.navigate(["/admin"])
+        }
+        console.log(data);
+      }
+      catch(err){
+        // if there an error
+        if(err.code){
+          alert("Try again user or password worng")
+        }
+        console.log(err);
+      }
+      console.log(this.myForm.form.value);
+    }
+  }
 }
+
+
+
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild("f") myForm:any
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
+
+  async onSub() {
+    console.log(this.myForm.form.value);
+    let user = this.myForm.form.value
+    // let result = await this.authFb.signUpNewUser(user);
+    // deleting firebase configuration and placing test ones
+    let result = {user:"test test",message:"test",code:"test"}
+    console.log(result);
+    if(result.user) {
+      // Todo add new record
+      alert('Sign up succesful')
+      // this.dbFire.addUser(user);
+      this.router.navigate(["/"])
+      setTimeout(() => {
+        window.location.reload();
+      },400)
+    }
+    if(result.code){
+      alert(result.message)
+    }
+    // result.user -> success
+    // result.code -> problem
+    return result;
+    //TODO: also add new record in db of firebase
+  }
+
 
 }
