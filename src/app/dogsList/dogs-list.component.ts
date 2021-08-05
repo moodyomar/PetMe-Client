a875ed25690b5b6935b1df1c927748609529efd9
moodyomar <lonak.secret@gmail.com>
2021-08-05 20:01:48 +0300

    fix mobile hovers in dogs page

--- src/app/dogsList/dogs-list.component.ts
+++ src/app/dogsList/dogs-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 // import {dogs_ar_json} from "../data/sample"
 import { ApiService } from '../services/api.service';
 import { DogsService } from '../services/dogs.service';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-temp-name',
@@ -10,14 +11,24 @@ import { DogsService } from '../services/dogs.service';
 })
 export class DogsListComponent implements OnInit {
   dogs_ar:any[] = [];
+isLoggedIn:boolean = false;
   // dogs_ar_json:any = dogs_ar_json;
-  constructor(private apiSer:ApiService,private dogsSer:DogsService) { }
+  constructor(private apiSer:ApiService,private dogsSer:DogsService,private userSer:UsersService) { }
 
 
   ngOnInit(): void {
 this.dogs_ar = this.dogsSer.getDogs();
 let url = `${this.apiSer.API_URL}/dogs/`
 this.dogsSer.doApiList(url)
+  }
+
+  ngDoCheck(): void {
+    if (localStorage["tok"]) {
+      this.isLoggedIn = true;
+      this.userSer.isLoggedIn = true;
+    } else {
+      this.isLoggedIn = false;
+    }
   }
 
 }
