e8995d24b97e2ee895ec59c78d321cda697488da
moodyomar <lonak.secret@gmail.com>
2021-08-12 12:29:12 +0300

    implement AOS animation library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos'
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  ngOnInit(){
+AOS.init({
+  duration: 800,
+});
+  }
 }
-
-// app.component.spec.ts אפשר למחוק
