15abff9712eed6481702a6280ce39d386a90d94a
moodyomar <lonak.secret@gmail.com>
2021-08-30 11:08:04 +0300

    add dogs added by user in admin panel

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService } from '../services/api.service';
+import { DogsService } from '../services/dogs.service';
 import { UsersService } from '../services/users.service';
 
 @Component({
@@ -8,29 +10,67 @@ import { UsersService } from '../services/users.service';
   styleUrls: ['./admin-panel.component.css']
 })
 export class AdminPanelComponent implements OnInit {
+    dogs_ar:any[] = [];
+    usersDogs:any[] = [];
   isLoggedIn = true;
   userInfo:any = {};
-  constructor(private router:Router,private userSer:UsersService) { }
+  displayUserDogs:Boolean = false;
+  showModal:boolean = false;
+  idDel:String = '';
 
-  ngOnInit(): void {
+  constructor(private router:Router,private userSer:UsersService,private dogsSer:DogsService,private apiSer:ApiService) { }
 
-    if (localStorage["tok"]) {
-      this.isLoggedIn = true;
-      this.userSer.isLoggedIn = true;
-      this.userSer.getUserInfo()
-      this.userInfo = this.userSer.userInfo
+ngOnInit(): void {
+this.dogs_ar = this.dogsSer.getDogs();
+let url = `${this.apiSer.API_URL}/dogs/`
+this.dogsSer.doApiList(url)
 
-      
-    } else {
-      this.isLoggedIn = false;
-      this.router.navigate(["/login"])
-    }
+if (localStorage["tok"]) {
+  this.isLoggedIn = true;
+  this.userSer.isLoggedIn = true;
+  this.userSer.getUserInfo()
+  this.userInfo = this.userSer.userInfo;
 
+setTimeout(() => {
+  this.usersDogs = this.dogs_ar.filter(dog => dog.user_id === this.userInfo._id)
+  console.log(this.usersDogs)
+}, 500);
+
+
+
+} else {
+  this.isLoggedIn = false;
+  this.userSer.isLoggedIn = false;
+}
+  }
+
+  showUserDogs():void{
+this.displayUserDogs = !this.displayUserDogs
   }
 
   ngDoCheck(): void {
-   
+    if (localStorage["tok"]) {
+      this.isLoggedIn = true;
+      this.userSer.isLoggedIn = true;
+    } else {
+      this.isLoggedIn = false;
+      this.userSer.isLoggedIn = false;
+      this.router.navigate(['/login'])
+    }
   }
+
+  ifYes():void{
+    let url = `${this.apiSer.API_URL}/dogs/${this.idDel}`
+    this.dogsSer.deleteDog(url)
+    this.showModal = !this.showModal
+    }
+    ifNo():void{
+      this.showModal = !this.showModal;
+    }
+      userDeleteRequest(_id:String):void{
+        this.showModal = !this.showModal
+        this.idDel = _id;
+      }
 
   
 }
