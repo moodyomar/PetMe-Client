2b9ee766f88ee29755b192677ba0b092c53c97fa
moodyomar <lonak.secret@gmail.com>
2021-08-30 12:50:36 +0300

    scrollToTop button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Event, NavigationStart, Router } from '@angular/router';
 import { UsersService } from '../services/users.service';
 
 
@@ -20,14 +21,21 @@ export class HeaderComponent implements OnInit {
     return (Math.floor((new Date).getTime() / 1000)) >= expiry;
   }
 
-  constructor(private userSer: UsersService,private scroll: ViewportScroller) { }
+  constructor(private userSer: UsersService,private scroll: ViewportScroller,private router: Router) { }
 
   ngOnInit(): void {
-    
+    this.router.events.subscribe((event:Event) => {
+      if(event instanceof NavigationStart){
+        this.scroll.scrollToPosition([0,0]);
+      }
+    })
   }
 
   scrolldown(){
     this.scroll.scrollToPosition([0,450])
+}
+  scrollTop(){
+    this.scroll.scrollToPosition([0,0])
 }
 
   ngDoCheck(): void {
@@ -38,11 +46,14 @@ export class HeaderComponent implements OnInit {
       this.isLoggedIn = false;
       localStorage.removeItem('tok')
     }
+    
   }
 
 
 logOut():void{
   this.userSer.logOut()
 }
+
+
 
 }
